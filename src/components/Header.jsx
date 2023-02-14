e4380cb37bd06f7baa9aa7af51863ceb7a29305f
Qazi-Saad <saadbinrafay@gmail.com>
2023-02-14 23:01:04 +0500

    second commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header-box">
+        <h1 id="heading">Todo App</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
